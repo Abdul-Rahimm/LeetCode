2604e992d9d91a90f542d19ec047a3f96146affa
Abdul-Rahimm <k213951@nu.edu.pk>
2023-10-07 20:13:16 +0500

    7 oct 2023

--- October/7. 1420h. Build Array Where You Can Find The Maximum Exactly K Comparisons.cpp
+++ October/7. 1420h. Build Array Where You Can Find The Maximum Exactly K Comparisons.cpp
@@ -6,9 +6,42 @@ typedef vector<int> vi;
 typedef vector<vi> vvi;
  
 
- int numOfArrays(int n, int m, int k) {
-        
+int MOD = 1e9 + 7;
+int N,M,K;
+int dp[51][51][101];
+    
+int solve(int idx,int searchcost,int maxsofar){
+    if(idx == N){
+        if(searchcost == K)
+            return 1;
+        return 0;
     }
+
+    if(dp[idx][searchcost][maxsofar] != -1)
+        return dp[idx][searchcost][maxsofar];
+    
+    
+    int result = 0;
+    for(int i = 1; i <= M ; i++){
+        
+        if(i > maxsofar)
+            result += (solve(idx+1,searchcost+1,i)) % MOD;
+        else
+            result += (solve(idx+1,searchcost,maxsofar)) % MOD;
+
+    }
+    
+    return dp[idx][searchcost][maxsofar] = result % MOD;   
+}
+
+int numOfArrays(int n, int m, int k) {
+    N=n;
+    M=m;
+    K=k;
+    
+    memset(dp,-1,sizeof(dp));
+    return solve(0,0,0);
+}
 int main(){
 
 
