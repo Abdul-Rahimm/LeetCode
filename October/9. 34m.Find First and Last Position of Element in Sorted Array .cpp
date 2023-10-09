fc54eaf184c594faccbbf6b930da2aa4e40e22eb
Abdul-Rahimm <k213951@nu.edu.pk>
2023-10-09 18:16:42 +0500

    initial commit - 9 october

---
+++ October/9. 34m.Find First and Last Position of Element in Sorted Array .cpp
@@ -0,0 +1,66 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) x.begin(), x.end()
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+void print(vi nums){
+    for(int i : nums)
+        cout << i << " ";
+}
+
+int n;
+int search_left_most(vi &nums, int target){
+    int left_most = -1;
+
+    int l = 0 , r = n-1 ;
+    int mid = (l + (r-l))/2;
+
+    while(l <= r){
+        if(nums[mid] == target){
+            left_most= mid;
+            r = mid-1;
+        }
+        else if(nums[mid] > target)
+            r = mid-1;
+        else   
+            l = mid+1;
+    }
+
+    return left_most;
+}
+int search_right_most(vi &nums, int target){
+      int r_most = -1;
+
+    int l = 0 , r = n-1 ;
+    int mid = (l + (r-l))/2;
+
+    while(l <= r){
+        if(nums[mid] == target){
+            r_most= mid;
+            l = mid+1;
+        }
+        else if(nums[mid] > target)
+            r = mid-1;
+        else   
+            l = mid+1;
+    }
+
+    return r_most;
+}
+vector<int> searchRange(vector<int>& nums, int target) {
+    n = nums.size();
+
+    int left_most  = search_left_most(nums,target);
+    int right_most = search_right_most(nums,target);
+
+    return {left_most,right_most};
+}
+
+int main(){
+    vi nums{5,7,7,8,8,10};
+    vi ans = searchRange(nums,8);
+    print(ans);
+
+
+}
