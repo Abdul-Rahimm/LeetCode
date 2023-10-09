099b1fd908cacb7c4c0210f19f5bea6f0c5b8dcf
Abdul-Rahimm <k213951@nu.edu.pk>
2023-10-09 22:12:38 +0500

    correct version of question

--- October/9. 34m.Find First and Last Position of Element in Sorted Array .cpp
+++ October/9. 34m.Find First and Last Position of Element in Sorted Array .cpp
@@ -11,47 +11,48 @@ void print(vi nums){
 
 int n;
 int search_left_most(vi &nums, int target){
+    int l = 0, r = n-1;
+
     int left_most = -1;
-    int num_mid;
-    int l = 0 , r = n-1 ;
-    
 
     while(l <= r){
-        int mid = (l + (r-l))/2;
-        num_mid = nums[mid];
-        
+        int mid = l + (r-l)/2;
+        int num_mid = nums[mid];
+
         if(num_mid == target){
-            left_most= mid;
+            left_most = mid;
             r = mid-1;
         }
-        else if(num_mid > target)
-            r = mid-1;
-        else   
+        else if(num_mid < target)
             l = mid+1;
+        else    
+            r = mid-1;
+
     }
 
     return left_most;
 }
 int search_right_most(vi &nums, int target){
-    int r_most = -1;
-    int num_mid;
-    int l = 0 , r = n-1 ;
+    int l = 0, r = n-1;
+
+    int right_most = -1;
 
     while(l <= r){
-        int mid = (l + (r-l))/2;
-        num_mid = nums[mid];
+        int mid = l + (r-l)/2;
+        int num_mid = nums[mid];
 
         if(num_mid == target){
-            r_most= mid;
+            right_most = mid;
             l = mid+1;
         }
-        else if(num_mid > target)
-            r = mid-1;
-        else   
+        else if(num_mid < target)
             l = mid+1;
+        else    
+            r = mid-1;
+
     }
 
-    return r_most;
+    return right_most;
 }
 vector<int> searchRange(vector<int>& nums, int target) {
     n = nums.size();
@@ -60,6 +61,7 @@ vector<int> searchRange(vector<int>& nums, int target) {
     int right_most = search_right_most(nums,target);
 
     return {left_most,right_most};
+    // return {left_most,0};
 }
 
 int main(){
