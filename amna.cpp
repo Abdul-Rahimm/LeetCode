e796f4d94e969f244a464cf0bcb5316bfc0666f8
Abdul-Rahimm <k213951@nu.edu.pk>
2023-10-09 13:37:32 +0500

    removed  mul

--- amna.cpp
+++ amna.cpp
@@ -4,16 +4,12 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 typedef vector<int> vi;
 typedef vector<vi> vvi;
-int mul(int a,int b){
-    return a*b;
-}
+
 int add(int a,int b){
     return a+b;
 }
 
 int main(){
    cout << add(2,3) << endl;
-   cout << mul(2,3) << endl;
-   
 
 }
