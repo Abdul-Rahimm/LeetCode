be0b915e6bdb5ea4299214987e57122cdc3d7147
Abdul-Rahimm <k213951@nu.edu.pk>
2023-10-13 18:44:10 +0500

    backtracking question DS lab mid. some amendments.

---
+++ 10. DS lab Mid/lab2.cpp
@@ -0,0 +1,81 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) x.begin(), x.end()
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+int rows,cols;
+
+vector<pair<int,int>> dir{{0,1},{0,-1},{1,0},{-1,0}, 
+                            {-1,-1},{-1,1},{1,-1},{1,1}};
+
+bool helper(vector<vector<char>> grid, string word, int idx,int i, int j, int word_size,vector<vector<bool>> &vis){ 
+    if(i >= rows || j >= cols || i < 0 || j < 0 || word_size > word.size())
+        return false;   //out of bound
+    
+    if(vis[i][j] == true)
+        return false;       //cant go back from where came
+    vis[i][j] = true;       //mark true
+    
+    if(word_size == word.size()-1 && word[idx] == grid[i][j])
+        return true;
+    
+    for(auto pair : dir){
+        int new_i = pair.first;
+        int new_j = pair.second;
+
+        if(helper(grid,word,idx+1,i + new_i,j + new_j,word_size+1,vis))
+            return true;
+    }
+
+    return false;
+}
+
+bool solver(vector<vector<char>> grid, string word){
+    for(int i = 0 ; i < rows ; i++){
+        for(int j = 0 ; j < cols ; j++){
+
+            if(word[0] == grid[i][j]){
+                vector<vector<bool>> vis(rows, vector<bool>(cols, false));
+
+                if(helper(grid,word,0,i,j,0,vis))
+                    return true;
+
+            }
+
+        }
+    }
+
+    return false;
+}
+
+vector<string> solve(vector<vector<char>> grid, vector<string> dict){
+    vector<string> ret;
+
+    for(string word: dict){
+        if(solver(grid,word))
+            ret.push_back(word);
+    }
+
+    return ret;
+}
+
+template <typename T>
+void print(vector<T>& vec) {
+    for (T item : vec) 
+        cout << item << " ";
+    
+    cout << endl;
+}
+int main(){
+    vector<vector<char>> grid {{'c','a','t'},{'r','r','e'},{'t','o','n'}};
+    vector<string> dict{"cat","cater","moon","art","toon","not","eat","ton","tan"};
+
+    rows = grid.size();
+    cols = grid[0].size();
+
+    vector<string> ans = solve(grid,dict);
+    print(ans);
+
+
+}
