1b9ef1904aa9fdc06ac742cf10bf2c878d57127f
Abdul-Rahimm <k213951@nu.edu.pk>
2023-11-02 22:51:59 +0500

    2 november. count nodes equal to avg of subtree

--- 8. Novermber/2. 2265m. Count Nodes Equal to Average of Subtree.cpp
+++ 8. Novermber/2. 2265m. Count Nodes Equal to Average of Subtree.cpp
@@ -1,20 +1,81 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//Definition for a binary tree node.
-struct TreeNode {
+
+// A binary tree node has data, pointer to left child and a pointer to right child
+struct node {
     int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    struct node* left;
+    struct node* right;
 };
 
-int averageOfSubtree(TreeNode* root) {
-    
+// Helper function that allocates a new node with the given data and NULL left and right pointers.
+struct node* newNode(int data){
+    struct node* node = (struct node*)malloc(sizeof(struct node));
+    node->val = data;
+    node->left = NULL;
+    node->right = NULL;
+ 
+    return (node);
 }
 
-int main(){
+int BFS_return(node* root){
+        int val = 0;
+        int count = 0;
+        queue<node*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            node* fr = q.front();
+            q.pop();
+            
+            val += fr->val;
+            count++;
+
+            if(fr->left)
+                q.push(fr->left);
+            if(fr->right)
+                q.push(fr->right);
+        }
+        return val/count;
+    }
+    
+    int BFS(node* root){
+        int count = 0;
+        
+        queue<node*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            node* fr = q.front();
+            q.pop();
+            
+            if(fr->val == BFS_return(fr))
+                count++;
+            
+            if(fr->left)
+                q.push(fr->left);
+            if(fr->right)
+                q.push(fr->right);
+        }
+        
+        return count;
+    }
+    
+    int averageOfSubtree(node* root) {
+       
+        return BFS(root);
+    }
+
+int main(){    
+    struct node* root = newNode(4);
+    root->left = newNode(8);
+    root->right = newNode(5);
+    root->left->left = newNode(0);
+    root->left->right = newNode(1);
+    root->right->right = newNode(6);
+
+    cout << averageOfSubtree(root) << endl; 
+
     
 }
