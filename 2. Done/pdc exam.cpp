c137421a0bd6b9f8eb11286d9fc1bd8723f0e898
Abdul-Rahimm <k213951@nu.edu.pk>
2023-11-06 14:56:12 +0500

    PDC qs1. 100%algo 0%pdc

---
+++ 2. Done/pdc exam.cpp
@@ -0,0 +1,35 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) x.begin(), x.end()
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+ 
+int solve(vi nums,int start,int end){
+    
+    if(end-start > 2){
+        int mid = start + (end-start)/2;
+        int a = solve(nums,start,mid);
+        int b = solve(nums,mid+1,end);
+
+        return min(a,b);
+    }
+
+    return min(nums[start],nums[end]);
+
+}
+
+int main(){
+    int t;
+    cin >> t;
+    vector<int> nums(t);
+
+    for(int i = 0 ; i < t ; i++){
+        cin >> nums[i];
+    }
+
+    int ans = solve(nums,0,t-1);
+
+    cout << ans;
+
+}
