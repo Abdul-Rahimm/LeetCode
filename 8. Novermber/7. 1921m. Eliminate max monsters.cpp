ce5f4a0992fbad386d449f3ae8998be4954340c0
Abdul-Rahimm <k213951@nu.edu.pk>
2023-11-08 02:20:10 +0500

    7 october final version

--- 8. Novermber/7. 1921m. Eliminate max monsters.cpp
+++ 8. Novermber/7. 1921m. Eliminate max monsters.cpp
@@ -4,40 +4,27 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 typedef vector<int> vi;
 typedef vector<vi> vvi;
-void print(vi ans){
-    for(int i:ans)
-        cout << i << " ";
-} 
-void print(priority_queue<int, vector<int> , greater<int>> pq){
-    while(!pq.empty()){
-        cout << pq.top() << " ";
-        pq.pop();
-    }
-}
+
 
 int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
         int n = dist.size();
         vector<int> diff(n);
-        priority_queue<int, vector<int> , greater<int>> pq;
         
         for(int i = 0 ; i < n ; i++){
-            diff[i] = dist[i]/speed[i] ;
+            diff[i] = ceil( (float)dist[i] / speed[i]) ;
         }
         
-        for(int i = 0 ; i < n ; i++){
-            pq.push(diff[i]);
-        }
+        sort(diff.begin(),diff.end());
 
-        pq.pop();
         int count = 1;
-        while(!pq.empty() && count != n){
-            int top = pq.top();
-            pq.pop();
+        int time  = 1;
 
-            if(top > count)
-                count++;
-            else    
+        for(int i = 1; i < n ; i++){
+            if(diff[i] - time <= 0)
                 return count;
+
+                count++;
+                time++;
         }
 
         return count;
