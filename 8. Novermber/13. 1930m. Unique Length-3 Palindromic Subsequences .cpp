2d50c5a68fe0b39227cda272bcf33181e897cf99
Abdul-Rahimm <k213951@nu.edu.pk>
2023-11-14 23:10:08 +0500

    13 november. MIK version. passes all test cases.

--- 8. Novermber/13. 1930m. Unique Length-3 Palindromic Subsequences .cpp
+++ 8. Novermber/13. 1930m. Unique Length-3 Palindromic Subsequences .cpp
@@ -29,11 +29,45 @@ int helper(string s, int len, int idx, string word){
     return take + notTake;
 
 }
-int countPalindromicSubsequence(string s) {
+int countPalindromicSubsequence_me(string s) {
     N = s.size();
     return helper(s, 3, 0, "");
 }
 
+int countPalindromicSubsequence(string s){
+    int n = s.size();
+
+    unordered_set<char> letters;
+
+    for(char i : s)
+        letters.insert(i);
+
+    int ans = 0;
+    for(char letter : letters){
+
+         int left = -1;
+        int right = -1;
+
+        for(int i = 0 ; i < n ; i++){
+
+            if(s[i] == letter){
+                if(left == -1)
+                    left = i;
+
+                right = i;
+            }
+        }
+
+        unordered_set<char> uni;
+        for(int middle = left+1 ; middle < right ; middle++){
+            uni.insert(s[middle]);
+        }
+
+        ans += uni.size();
+    }
+
+    return ans;
+}
 int main(){
     string a = "maaz";
     cout << countPalindromicSubsequence(a) << endl; 
