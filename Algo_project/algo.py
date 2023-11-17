6f3bd4e98de5da10a89cd443c14810a76463d675
Abdul-Rahimm <k213951@nu.edu.pk>
2023-11-18 03:03:05 +0500

    two methods working fine

--- Algo_project/algo.py
+++ Algo_project/algo.py
@@ -1,39 +1,78 @@
-import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import Canvas
+import time
 
-class CoordinateSystem:
-    def __init__(self):
-        self.coordinates = []
-        self.fig, self.ax = plt.subplots()
-        self.ax.set(title='Click on the Coordinate System to Enter Points',
-                    xlabel='X-axis', ylabel='Y-axis')
-        self.cid = self.fig.canvas.mpl_connect('button_press_event', self.onclick)
+# Function to check if lines intersect
+def check_intersection(a, b, c, d):
+    def orientation(p, q, r):
+        val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
+        if val == 0:
+            return 0  # Collinear
+        return 1 if val > 0 else 2  # Clockwise or counterclockwise
 
-    def onclick(self, event):
-        if event.inaxes == self.ax:
-            x, y = event.xdata, event.ydata
-            self.coordinates.append((x, y))
-            self.ax.plot(x, y, 'ro')  # Plot the clicked point
-            plt.draw()
+    def on_segment(p, q, r):
+        return (q[0] <= max(p[0], r[0]) and q[0] >= min(p[0], r[0]) and
+                q[1] <= max(p[1], r[1]) and q[1] >= min(p[1], r[1]))
 
-            if len(self.coordinates) == 2:
-                # Draw a line between the first and second pairs of points
-                self.ax.plot([self.coordinates[0][0], self.coordinates[1][0]],
-                             [self.coordinates[0][1], self.coordinates[1][1]], 'b-')
-                plt.draw()
+    o1 = orientation(a, b, c)
+    o2 = orientation(a, b, d)
+    o3 = orientation(c, d, a)
+    o4 = orientation(c, d, b)
 
-                # Print the coordinates
-                print("Entered Coordinates:")
-                for i, coord in enumerate(self.coordinates):
-                    print(f"Point {i + 1}: ({coord[0]}, {coord[1]})")
+    if o1 != o2 and o3 != o4:
+        return True, None  # Lines intersect
 
-                # Disconnect the click event after two points are entered
-                self.fig.canvas.mpl_disconnect(self.cid)
+    # Special cases for collinear lines
+    if o1 == 0 and on_segment(a, c, b):
+        return True, (c[0], c[1])  # Intersection point is c
+    if o2 == 0 and on_segment(a, d, b):
+        return True, (d[0], d[1])  # Intersection point is d
+    if o3 == 0 and on_segment(c, a, d):
+        return True, (a[0], a[1])  # Intersection point is a
+    if o4 == 0 and on_segment(c, b, d):
+        return True, (b[0], b[1])  # Intersection point is b
 
-    def show_plot(self):
-        plt.show()
+    return False, None  # No intersection
 
-# Create an instance of the CoordinateSystem class
-coordinate_system = CoordinateSystem()
+# Function to get user input via mouse clicks
+def get_coordinates():
+    root = tk.Tk()
+    root.title('Click on the Canvas to Enter Points')
 
-# Display the plot
-coordinate_system.show_plot()
+    canvas = Canvas(root, width=500, height=500)
+    canvas.pack()
+
+    coordinates = []
+
+    def on_click(event):
+        nonlocal coordinates
+        x, y = event.x, event.y
+        print(f"Clicked at ({x}, {y})")
+        coordinates.append((x, y))
+        canvas.create_oval(x - 5, y - 5, x + 5, y + 5, fill='red')  # Mark the point
+
+        if len(coordinates) == 2:
+            canvas.create_line(coordinates[0][0], coordinates[0][1], coordinates[1][0], coordinates[1][1], fill='blue', width=2)
+
+        if len(coordinates) == 4:
+            canvas.create_line(coordinates[2][0], coordinates[2][1], coordinates[3][0], coordinates[3][1], fill='green', width=2)
+            root.after(2000, root.destroy)  # Close the window after 2 seconds
+
+    canvas.bind("<Button-1>", on_click)
+
+    root.mainloop()
+
+    return coordinates
+
+# Get input coordinates from the user
+user_coordinates = get_coordinates()
+
+# Check if lines intersect
+intersect, intersection_point = check_intersection(user_coordinates[0], user_coordinates[1],
+                                                   user_coordinates[2], user_coordinates[3])
+
+# Report the result
+if intersect:
+    print("Lines intersect.")
+else:
+    print("Lines do not intersect.")
