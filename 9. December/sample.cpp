db571767218643d4599c4db46179d62c1350f2a6
Abdul-Rahimm <k213951@nu.edu.pk>
2023-12-23 03:05:28 +0500

    learned new function from MIK. added here

--- 9. December/sample.cpp
+++ 9. December/sample.cpp
@@ -5,10 +5,15 @@ using namespace std;
 typedef vector<int> vi;
 typedef vector<vi> vii;
  
-
-
+int func(string str){
+    return count(str.begin(),str.end(), '0');
+}
+//these changes are being done from the new-function branch
 int main(){
 
     //just the main function
+
+    string str = "100111001";
+    cout << func(str) << endl;
 
 }
